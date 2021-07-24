104da8a3e53a5c91122a7fa51a6ee9af2b3d75c7
Myles Shannon <mylios101@gmail.com>
2021-07-24 16:18:19 -0500

    Initial React project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Modals in React demonstration</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
