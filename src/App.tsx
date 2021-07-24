cd5146cc23242bb821b81402d58088eee47e2f9e
Myles Shannon <mylios101@gmail.com>
2021-07-24 16:57:52 -0500

    Add react-modal library

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import './App.css';
+import { Component } from "react";
+import Modal from "react-modal";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Modals in React demonstration</p>
-      </header>
-    </div>
-  );
+interface AppProps {}
+interface AppState {
+  isModalOpen: boolean
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      isModalOpen: false
+    };
+  }
+
+  setModal(isModalOpen: boolean): void {
+    this.setState({ isModalOpen });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={() => this.setModal(true)}>Open modal</button>
+        <Modal
+          isOpen={this.state.isModalOpen}
+          onRequestClose={() => this.setModal(false)}
+          style={{
+            overlay: {
+              backgroundColor: "gray"
+            }
+          }}
+        >
+          <button onClick={() => this.setModal(false)}>Close</button>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
